c262ef74af137307c77290adec8e9c1504893212
Parashar <parasharsangle@gmail.com>
2022-08-23 11:33:15 +0530

    Add: slider, legend

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ height = document.documentElement.clientHeight - margin.top - margin.bottom;
 
 delay = 250
 
-yearMin = d3.min(data, d => d.year)
+const [yearMin, yearMax] = d3.extent(data, d => d.year)
+var timer;
 
 yearStep = 1
 
@@ -41,9 +42,7 @@ yAxis = g => g
     .attr("text-anchor", "end")
     .text(data.y))
 
-
-
-let svg = d3.select("svg").attr("viewBox", [0, 0, width, height]);
+const svg = d3.select("svg").attr("viewBox", [0, 0, width, height]);
 
 svg.append("g")
   .call(xAxis);
@@ -53,7 +52,6 @@ svg.append("g")
 
 const group = svg.append("g");
 
-let rect = group.selectAll("rect");
 
 const update = (year) => {
   const dx = x.step() * (year - yearMin) / yearStep;
@@ -62,12 +60,15 @@ const update = (year) => {
     .ease(d3.easeLinear)
     .duration(delay);
 
+  let rect = group.selectAll("rect");
+
   rect = rect
     .data(data.filter(d => d.year === year), d => `${d.sex}:${d.year - d.age}`)
     .join(
       enter => enter.append("rect")
         .style("mix-blend-mode", "darken")
         .attr("fill", d => color(d.sex))
+        .attr("data-legend", function (d) { return d.age })
         .attr("x", d => x(d.age) + dx)
         .attr("y", d => y(0))
         .attr("width", x.bandwidth() + 1)
@@ -86,4 +87,161 @@ const update = (year) => {
     .attr("transform", `translate(${-dx},0)`);
 };
 
-update(yearMin)
+update(yearMin);
+
+const applyLegend = (legendKeys) => {
+  //Initialize legend
+  var legendItemSize = 12;
+  var legendSpacing = 4;
+  var xOffset = 0;
+  var yOffset = 0;
+  const legend = svg
+    // .append('#legend')
+    .append('g')
+    .attr('class', 'legendContainer')
+    .selectAll('.legendItem')
+    .data(legendKeys);
+
+  //Create legend items
+  legend
+    .enter()
+    .append('rect')
+    .attr('class', 'legendItem')
+    .attr('width', legendItemSize)
+    .attr('height', legendItemSize)
+    .style("mix-blend-mode", "darken")
+    .style('fill', d => color(d))
+    .attr('transform',
+      (d, i) => {
+        var x = xOffset;
+        var y = yOffset + (legendItemSize + legendSpacing) * i;
+        return `translate(${x}, ${y})`;
+      });
+
+  //Create legend labels
+  legend
+    .enter()
+    .append('text')
+    .attr('x', xOffset + legendItemSize + 5)
+    .attr('y', (d, i) => yOffset + (legendItemSize + legendSpacing) * i + 12)
+    .text(d => d);
+
+}
+
+applyLegend(['M', 'F']);
+
+
+
+
+////////// slider //////////
+const sliderMargin = ({ top: 20, right: 20, bottom: 20, left: 20 });
+
+var formatDateIntoYear = d3.timeFormat("%Y");
+var formatDate = d3.timeFormat("%b %Y");
+var parseDate = d3.timeParse("%Y");
+
+
+var moving = false;
+var currentValue = 0;
+var targetValue = width;
+
+var playButton = d3.select("#play-button");
+
+var sliderX = d3.scaleTime()
+  .domain([parseDate(yearMin), parseDate(yearMax)])
+  .range([sliderMargin.left, width - sliderMargin.right - sliderMargin.left])
+  .clamp(true);
+
+console.log(sliderX.domain());
+
+var slider = svg.append("g")
+  .attr("class", "slider")
+  .attr("transform", `translate(${sliderMargin.left},${sliderMargin.top})`)
+
+slider.append("line")
+  .attr("class", "track")
+  .attr("x1", sliderX.range()[0])
+  .attr("x2", sliderX.range()[1])
+  .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+  .attr("class", "track-inset")
+  .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+  .attr("class", "track-overlay")
+  .call(d3.drag()
+    .on("start.interrupt", function () { slider.interrupt(); })
+    .on("start drag", function (event) {
+      currentValue = event.x;
+      const selectedYear = formatDateIntoYear(sliderX.invert(currentValue));
+      updateSlider(selectedYear);
+      console.log((selectedYear));
+      update(selectedYear);
+    })
+  );
+
+slider.insert("g", ".track-overlay")
+  .attr("class", "ticks")
+  .attr("transform", `translate(${sliderMargin.left},${sliderMargin.top})`)
+  .selectAll("text")
+  .data(sliderX.ticks(10))
+  .enter()
+  .append("text")
+  .attr("x", sliderX)
+  .attr("y", 10)
+  .attr("text-anchor", "middle")
+  .text(function (d) {
+    return formatDateIntoYear(d);
+  });
+
+var handle = slider.insert("circle", ".track-overlay")
+  .attr("class", "handle")
+  .attr("cx", sliderX.range()[0])
+  .attr("r", 9);
+
+var label = slider.append("text")
+  .attr("class", "label")
+  .attr("text-anchor", "middle")
+  .text(formatDate(yearMin))
+  .attr("transform", `translate(${sliderMargin.left},${sliderMargin.top})`)
+
+function updateSlider(year) {
+  // update position and text of label according to slider scale
+  const formattedYear = parseDate(year)
+  handle
+    .attr("cx", sliderX(formattedYear));
+
+  label
+    .attr("x", sliderX(formattedYear))
+    .text(year);
+}
+
+// play //
+playButton
+  .on("click", function () {
+    var button = d3.select(this);
+    if (button.text() == "Pause") {
+      moving = false;
+      clearInterval(timer);
+      button.text("Play");
+    } else {
+      moving = true;
+      timer = setInterval(step, 500);
+      button.text("Pause");
+    }
+    console.log("Slider moving: " + moving);
+  })
+
+// TODO: needs some work
+function step() {
+  const selectedYear = formatDateIntoYear(sliderX.invert(currentValue));
+  updateSlider(selectedYear);
+  update(selectedYear);
+
+  currentValue = currentValue + (targetValue / 151);
+  if (currentValue > targetValue) {
+    moving = false;
+    currentValue = 0;
+    clearInterval(timer);
+    // timer = 0;
+    playButton.text("Play");
+    console.log("Slider moving: " + moving);
+  }
+};
